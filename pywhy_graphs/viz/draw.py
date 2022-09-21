b5bfa64547786b69fc44f8af8bebd45575656061
Adam Li <adam2392@gmail.com>
2022-09-21 08:30:35 -0400

    [DOC] Adding basic conceptual example of causal graphs (#11)
    * Adding example of causal graphs and walking through their concepts
    * Add pag example
    * Fix drawing function to work for CPDAGs and PAGs
    
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- pywhy_graphs/viz/draw.py
+++ pywhy_graphs/viz/draw.py
@@ -25,33 +25,50 @@ def draw(G: nx.MixedEdgeGraph, direction: Optional[str] = None):
     # set direction from left to right if that's preferred
     if direction == "LR":
         dot.graph_attr["rankdir"] = direction
-
     shape = "square"  # 'plaintext'
+
+    found_circle_sibs = set()
+    if hasattr(G, "circle_edges"):
+        for sib1, sib2 in G.circle_edges:
+            # memoize if we have seen the bidirected circular edge before
+            if f"{sib1}-{sib2}" in found_circle_sibs or f"{sib2}-{sib1}" in found_circle_sibs:
+                continue
+            found_circle_sibs.add(f"{sib1}-{sib2}")
+
+            # set directionality of the edges
+            dir = "forward"
+            if (sib2, sib1) in G.circle_edges:
+                dir = "both"
+                arrowtail = "odot"
+            elif (sib2, sib1) in G.directed_edges:
+                dir = "both"
+                arrowtail = "normal"
+            sib1, sib2 = str(sib1), str(sib2)
+            dot.edge(sib1, sib2, arrowhead="odot", arrowtail=arrowtail, dir=dir, color="green")
+
     for v in G.nodes:
         child = str(v)
 
         dot.node(child, shape=shape, height=".5", width=".5")
 
         for parent in G.predecessors(v):
+            # memoize if we have seen the bidirected circular edge before
+            if f"{child}-{parent}" in found_circle_sibs or f"{parent}-{child}" in found_circle_sibs:
+                continue
             parent = str(parent)
             if parent == v:
                 dot.edge(parent, child, style="invis")
             else:
                 dot.edge(parent, child, color="blue")
 
-        if hasattr(G, "undirected_edges"):
-            for neb1, neb2 in G.undirected_edges:
-                neb1, neb2 = str(neb1), str(neb2)
-                dot.edge(neb1, neb2, dir="none", color="brown")
+    if hasattr(G, "undirected_edges"):
+        for neb1, neb2 in G.undirected_edges:
+            neb1, neb2 = str(neb1), str(neb2)
+            dot.edge(neb1, neb2, dir="none", color="brown")
 
-        if hasattr(G, "bidirected_edges"):
-            for sib1, sib2 in G.bidirected_edges:
-                sib1, sib2 = str(sib1), str(sib2)
-                dot.edge(sib1, sib2, dir="both", color="red")
-
-        if hasattr(G, "circle_edges"):
-            for sib1, sib2 in G.circle_edges:
-                sib1, sib2 = str(sib1), str(sib2)
-                dot.edge(sib1, sib2, arrowhead="circle", color="green")
+    if hasattr(G, "bidirected_edges"):
+        for sib1, sib2 in G.bidirected_edges:
+            sib1, sib2 = str(sib1), str(sib2)
+            dot.edge(sib1, sib2, dir="both", color="red")
 
     return dot
