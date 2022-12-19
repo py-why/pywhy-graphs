d30ba7b926ae351894d198938e625d8cccd6a47a
Julien Siebert <83209746+siebert-julien@users.noreply.github.com>
2022-12-19 08:58:18 +0100

    Update examples/draw_and_compare_graphs_with_same_layout.py
    Co-authored-by: Adam Li <adam2392@gmail.com>
    Signed-off-by: Julien Siebert <83209746+siebert-julien@users.noreply.github.com>
    Signed-off-by: Julien Siebert <julien.siebert@iese.fraunhofer.de>

--- examples/draw_and_compare_graphs_with_same_layout.py
+++ examples/draw_and_compare_graphs_with_same_layout.py
@@ -6,7 +6,9 @@ Drawing graphs and setting their layout for visual comparison
 =============================================================
 
 One can draw a graph without setting the `pos` argument, in that case graphviz will choose how to place the nodes.
-See https://graphviz.readthedocs.io/en/stable/api.html?highlight=render#graphviz.Digraph.render
+See https://graphviz.readthedocs.io/en/stable/api.html?highlight=render#graphviz.Digraph.render.
+In this example, we demonstrate how to visualize various different graphs with a fixed layout
+for the nodes, so that they are easily comparable.
 
 
 This examples shows how to create a position layout for all the nodes (using networkx)
