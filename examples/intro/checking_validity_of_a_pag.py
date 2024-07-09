4a570a85bd701884fd631633300d39384399a113
Aryan Roy <aryanroy5678@gmail.com>
2024-07-09 20:09:48 +0530

    Completed the example
    Signed-off-by: Aryan Roy <aryanroy5678@gmail.com>

--- examples/intro/checking_validity_of_a_pag.py
+++ examples/intro/checking_validity_of_a_pag.py
@@ -16,6 +16,9 @@ More details on inducing paths can be found at :footcite:`Zhang2008`.
 
 """
 
+import pywhy_graphs
+from pywhy_graphs.viz import draw
+from pywhy_graphs import PAG
 
 try:
     from dodiscover import FCI, make_context
@@ -25,6 +28,40 @@ except ImportError as e:
     raise ImportError(
         "The 'dodiscover' package is required to convert a MAG to a PAG."
     )
+
+
+# %%
+# PAGs in pywhy-graphs
+# ---------------------------
+# Constructing a PAG in pywhy-graphs is an easy task since
+# the library provides a separate class for this purpose.
+# True to the definition of PAGs, the class can contain
+# directed edges, bidirected edges, undirected edges and
+# cicle edges. To illustrate this, we construct an example PAG:
+
+pag = PAG()
+pag.add_edge("A", "B", pag.directed_edge_name)
+pag.add_edge("B", "A", pag.circle_edge_name)
+pag.add_edge("B", "C", pag.circle_edge_name)
+pag.add_edge("C", "B", pag.directed_edge_name)
+pag.add_edge("A", "D", pag.circle_edge_name)
+pag.add_edge("D", "A", pag.circle_edge_name)
+
+
+# Finally, the graph looks like this:
+dot_graph = draw(pag)
+dot_graph.render(outfile="new_pag.png", view=True)
+
+
+# %%
+# Validity of a PAG
+# ---------------------------
+# To check if the constructed PAG is a valid one, we 
+# can simply do:
+
+
+# returns True
+print(pywhy_graphs.valid_pag(pag))
 
 
 # %%
