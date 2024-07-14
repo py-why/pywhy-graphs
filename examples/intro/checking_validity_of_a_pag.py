850152b45ddce968be76dc47d2b3f79b0ea4760f
Aryan Roy <aryanroy5678@gmail.com>
2024-07-14 11:17:11 +0530

    Completed the example
    Signed-off-by: Aryan Roy <aryanroy5678@gmail.com>

--- examples/intro/checking_validity_of_a_pag.py
+++ examples/intro/checking_validity_of_a_pag.py
@@ -12,7 +12,7 @@ PAGs model this relationship by displaying all common edge marks shared
 by all members in the equivalence class and displaying circles for those marks
 that are not common.
 
-More details on inducing paths can be found at :footcite:`Zhang2008`.
+More details on PAGs can be found at :footcite:`Zhang2008`.
 
 """
 
@@ -35,15 +35,18 @@ except ImportError as e:
 # the library provides a separate class for this purpose.
 # True to the definition of PAGs, the class can contain
 # directed edges, bidirected edges, undirected edges and
-# cicle edges. To illustrate this, we construct an example PAG:
+# cicle edges. To illustrate this, we construct an example PAG
+# as described in :footcite:`Zhang2008`, figure 4:
 
 pag = PAG()
-pag.add_edge("A", "B", pag.directed_edge_name)
-pag.add_edge("B", "A", pag.circle_edge_name)
-pag.add_edge("B", "C", pag.circle_edge_name)
-pag.add_edge("C", "B", pag.directed_edge_name)
-pag.add_edge("A", "D", pag.circle_edge_name)
-pag.add_edge("D", "A", pag.circle_edge_name)
+pag.add_edge("I", "S", pag.directed_edge_name)
+pag.add_edge("G", "S", pag.directed_edge_name)
+pag.add_edge("G", "L", pag.directed_edge_name)
+pag.add_edge("S", "L", pag.directed_edge_name)
+pag.add_edge("PSH", "S", pag.directed_edge_name)
+pag.add_edge("S", "PSH", pag.circle_edge_name)
+pag.add_edge("S", "G", pag.circle_edge_name)
+pag.add_edge("S", "I", pag.circle_edge_name)
 
 
 # Finally, the graph looks like this:
@@ -54,13 +57,27 @@ dot_graph.render(outfile="new_pag.png", view=True)
 # %%
 # Validity of a PAG
 # ---------------------------
-# To check if the constructed PAG is a valid one, we 
-# can simply do:
+# For a PAG to be valid, it must represent a valid 
+# equivalent class of MAGs. This can be verified by
+# turning the PAG into an MAG and then checking the 
+# validity of the MAG
+# To check if the constructed PAG is a valid one in
+# pywhy-graphs, we can simply do:
 
 
 # returns True
 print(pywhy_graphs.valid_pag(pag))
 
+# %%
+# If we want to test whether this algorithm
+# is working correctly or not, we can change
+# a single mark in the graph such that the PAG
+# does not represent the equivalent class of MAGs:
+
+pag.remove_edge("S","I",pag.circle_edge_name)
+
+# returns False
+print(pywhy_graphs.valid_pag(pag))
 
 # %%
 # References
